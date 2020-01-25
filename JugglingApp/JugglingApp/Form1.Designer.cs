9daf95159642c5d613b96be49be5efdf643ede05
mtotbagi <mtotbagi@users.noreply.github.com>
2020-01-25 07:53:17 +0100

    Aasd

--- JugglingApp/JugglingApp/Form1.Designer.cs
+++ JugglingApp/JugglingApp/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.sequenceText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -43,31 +43,29 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(250, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(325, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            // 
+            // sequenceText
+            // 
+            this.sequenceText.Location = new System.Drawing.Point(203, 46);
+            this.sequenceText.Name = "sequenceText";
+            this.sequenceText.Size = new System.Drawing.Size(100, 20);
+            this.sequenceText.TabIndex = 3;
+            this.sequenceText.TextChanged += new System.EventHandler(this.sequenceText_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.sequenceText);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -81,8 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sequenceText;
     }
 }
 
--- JugglingApp/JugglingApp/Form1.cs
+++ JugglingApp/JugglingApp/Form1.cs
@@ -12,6 +12,7 @@ namespace JugglingApp
 {
     public partial class Form1 : Form
     {
+
         public Pen pen;
         public Graphics formGraphics;
         public SolidBrush brush;
@@ -19,40 +20,69 @@ namespace JugglingApp
         public int screenHeight = Screen.PrimaryScreen.Bounds.Height;
         public int screenWidth = Screen.PrimaryScreen.Bounds.Width;
 
+        List<int> sequence;
+        int n;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            formGraphics.DrawArc(pen, 200, 200, 300, 500, 180, 180);
-            label1.Text = screenWidth.ToString();
-            label2.Text = screenHeight.ToString();
-        }
-        
-        void Draw()
-        {
-            for (int i = 0; i < 12; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    formGraphics.DrawEllipse(pen, 100 * i, screenHeight * 9 / 10, 25, 25);
-                }
-                else
-                {
-                    formGraphics.FillEllipse(brush, 100 * i, screenHeight * 9 / 10, 25, 25);
-                }
-            }
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
+            sequence = new List<int>();
+            n = 15;
             formGraphics = CreateGraphics();
             Color color = Color.Black;
             pen = new Pen(color, 3);
             brush = new SolidBrush(color);
-            Draw();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while(n <= 15)
+            {
+                for (int i = 0; i < sequence.Count; i++)
+                {
+                    DrawThrow(n, sequence[i]);
+                }
+            }
+            label2.Text = screenHeight.ToString();
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    formGraphics.DrawEllipse(pen, 100 * i + 200, screenHeight * 8 / 10, 25, 25);
+                }
+                else
+                {
+                    formGraphics.FillEllipse(brush, 100 * i + 200, screenHeight * 8 / 10, 25, 25);
+                }
+            }
+        }
+
+        void DrawThrow(int beat, int h)
+        {
+            formGraphics.DrawArc(pen, 100 * beat + 200 + 12, screenHeight * 8 / 10 - 75 * h + 12, 100 * h, 150 * h, 0, -180);
+        }
+
+        private void sequenceText_TextChanged(object sender, EventArgs e)
+        {
+            TextBox seqText = sender as TextBox;
+            if (seqText.Text.Length > 0)
+            {
+                if (Char.IsNumber(seqText.Text[seqText.Text.Length - 1]))
+                {
+                    sequence.Add(seqText.Text[seqText.Text.Length - 1]);
+                    label2.Text = sequence[0].ToString();
+                }
+                else
+                {
+                    label2.Text = "Error, please write only numbers";
+                }
+            
+                label2.Text += seqText.Text[seqText.Text.Length - 1];
+            }
         }
     }
 }
